7a133717a78ee208f3cb5697cc909b91dd4f4827
Cedric Feyaerts <c.feyaerts@captive.fr>
2021-08-25 11:41:08 +0200

    Improve notice compoenent

--- lib/alveole/previews/notice_component_preview.rb
+++ lib/alveole/previews/notice_component_preview.rb
@@ -1,15 +1,17 @@
 class NoticeComponentPreview < ViewComponent::Preview
+  include Alveole::Helper::ComponentHelper
+
   layout false
 
   def default
-    render(NoticeComponent.new(title: 'Example Alert', messages: ['Message 1', 'Message 2']))
+    component :notice, title: 'Example Alert', messages: ['Message 1', 'Message 2']
   end
 
   def without_messages
-    render(NoticeComponent.new(title: 'Example Notice'))
+    component :notice, :alert, title: 'Example Notice'
   end
 
-  def with_type_notice
-    render(NoticeComponent.new(title: 'Example Notice', messages: ['Message 1', 'Message 2'], type: 'notice'))
+  def with_alert
+    component :notice, :alert, title: 'Example Notice', messages: ['Message 1', 'Message 2']
   end
 end
