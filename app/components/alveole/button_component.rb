e665390db521e19805c09d3b1b128ebd897a5678
Clément Prod'homme <clement.prod-homme@captive.fr>
2024-01-30 00:02:23 +0100

    💄 Add 3 types of buttons

--- app/components/alveole/button_component.rb
+++ app/components/alveole/button_component.rb
@@ -1,15 +1,43 @@
 # frozen_string_literal: true
 
 class Alveole::ButtonComponent < ViewComponent::Base
-  def initialize(href: "#", disabled: false, color: :default, icon: nil)
+  CLASS_BY_TYPE = {
+    primary: {
+      base: "border",
+      colors: {
+        primary: "text-white bg-primary-500 hover:bg-primary-600 dark:hover:bg-primary-500 dark:bg-primary-600",
+        green: "text-white bg-green-500 hover:bg-green-600 dark:hover:bg-green-500 dark:bg-green-600"
+      }
+    },
+    secondary: {
+      base: "border",
+      colors: {
+        primary: "text-gray-700 bg-white hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800",
+        green: "text-gray-500 border-green-500 bg-white hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800",
+      }
+    },
+    tertiary: {
+      base: "",
+      colors: {
+        primary: "text-primary-700 focus:bg-gray-100 dark:text-gray-200 dark:focus:bg-gray-800",
+        green: "text-green-700 focus:bg-gray-100 dark:text-gray-200 dark:focus:bg-gray-800"
+      }
+    }
+  }
+  TYPES = [:primary, :secondary, :tertiary]
+
+  def initialize(href: "#", disabled: false, type: :default, color: :primary, icon: nil)
     @href = href
     @color = color
-    @class_color = CLASS_COLOR[color.to_sym]
+    @base_class = get_class(type, color)
     @icon = IconComponent.new(icon, class: "-ml-0.5 w-5 h-5") if icon
   end
 
-  CLASS_COLOR = {
-    default: "text-gray-700 bg-white hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800",
-    primary: "text-white bg-primary-500 hover:bg-primary-600 dark:hover:bg-primary-500 dark:bg-primary-600"
-  }
+  private
+
+  def get_class(type, color)
+    raise "Type invalide" unless TYPES.include?(type)
+
+    CLASS_BY_TYPE[type][:base] + " " + CLASS_BY_TYPE[type][:colors][color]
+  end
 end
--- test/components/previews/alveole/button_component_preview.rb
+++ test/components/previews/alveole/button_component_preview.rb
@@ -1,15 +1,51 @@
 # frozen_string_literal: true
 
 class Alveole::ButtonComponentPreview < Lookbook::Preview
-  def default
-    render(Alveole::ButtonComponent.new) do |_b|
+  # Buttons
+  # ------------
+  #
+  # - **Primary Buttons**: In UX design, primary buttons serve as the focal point for the most important actions. They are typically highlighted with vibrant colors or prominent placement to draw immediate attention. Primary buttons represent key actions like "Submit" in forms or "Buy Now" on product pages. Their distinctiveness helps users identify the primary action at a glance.
+  # - **Secondary Buttons**: Secondary buttons complement primary actions by offering alternative or less critical choices. They often represent actions like "Cancel" or "Back" and are presented with less visual prominence than primary buttons but still enough to be easily noticed. Secondary buttons provide users with additional options without overshadowing the primary action.
+  # - **Tertiary Buttons**: Tertiary buttons are used for less critical actions or less frequently accessed options. They are typically presented with a more subdued appearance, such as low-contrast buttons or links. Tertiary buttons help maintain a clean interface while still providing access to supplementary features or functions without cluttering the screen.
+  #
+  # In summary, the strategic use of primary, secondary, and tertiary buttons in UX design guides users effectively, highlighting key actions while offering alternative choices and additional options in an organized and accessible manner.
+  #
+  # @param color select { choices: [primary,green] }
+  # @param href text
+  # @param disabled toggle
+  # @!group Colors
+
+  def primary(color: :primary, href: "#", disabled: false)
+    render(Alveole::ButtonComponent.new(
+      type: :primary,
+      color: color,
+      href: href,
+      disabled: disabled
+    )) do |_b|
       "Button"
     end
   end
 
-  def primary
-    render(Alveole::ButtonComponent.new(color: :primary)) do |_b|
+  def secondary(color: :primary, href: "#", disabled: false)
+    render(Alveole::ButtonComponent.new(
+      type: :secondary,
+      color: color,
+      href: href,
+      disabled: disabled
+    )) do |_b|
       "Button"
     end
   end
+
+  def tertiary(color: :primary, href: "#", disabled: false)
+    render(Alveole::ButtonComponent.new(
+      type: :tertiary,
+      color: color,
+      href: href,
+      disabled: disabled
+    )) do |_b|
+      "Button"
+    end
+  end
+  # @!endgroup
 end
